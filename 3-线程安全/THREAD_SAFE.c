1c74facf0351ae6f792e32fad90bcd8b607d4f50
Dongska <104555286+dongska@users.noreply.github.com>
2023-01-19 22:56:28 +0800

    Version 0.0.2 线程锁解决访问冲突
    请根据代码的增减进行学习

--- 3-线程安全/THREAD_SAFE.c
+++ 3-线程安全/THREAD_SAFE.c
@@ -3,18 +3,27 @@
 #include<unistd.h>
 #include<pthread.h>
 
+/*version 0.0.1 实现线程冲突*/
+/*version 0.0.2 实现线程锁解决线程冲突*/
+
 int num;
+pthread_mutex_t lock; //定义线程锁
 
 void* thread_job1(void * arg)
 {
 	pthread_detach(pthread_self());
+
 	int tmp;
+	//lock??? 此处进行lock会让词线程占用大量使用num的时间片
 	for(int i = 0;i < 5000;i++)
 	{
+		pthread_mutex_lock(&lock); // 上锁
 		tmp = num;
 		printf("Thread [0x%x] Add num %d\n",(unsigned int)pthread_self(),++tmp);
 		num = tmp;
+		pthread_mutex_unlock(&lock); // 解锁
 	}
+	//unlock???
 	pthread_exit(0);
 }
 
@@ -25,12 +34,12 @@ void * thread_job2(void * arg)
 	int tmp;
 	for(int i = 0;i < 5000;i++)
 	{
+		pthread_mutex_lock(&lock); // 上锁
 		tmp = num;
 		printf("Thread [0x%x] Add num %d\n",(unsigned int)pthread_self(),++tmp);
 		num = tmp;
+		pthread_mutex_unlock(&lock); // 解锁
 	}
-	while(1)
-		sleep(1);
 	pthread_exit(0);
 }
 
@@ -38,6 +47,7 @@ void * thread_job2(void * arg)
 int main()
 {
 	pthread_t tids[2];
+	pthread_mutex_init(&lock,NULL); //初始化线程锁
 
 	for(int i=0;i<2;i++)
 	{
