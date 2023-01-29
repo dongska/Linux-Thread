32c13852a831f28c5d489053d219a4888babef49
Dongska <104555286+dongska@users.noreply.github.com>
2023-01-29 22:48:21 +0800

    进程锁完善 Ver 0.0.2

--- 3-线程安全/THREAD_SAFE_PROCESS_LOCK.c
+++ 3-线程安全/THREAD_SAFE_PROCESS_LOCK.c
@@ -11,7 +11,7 @@
 
 /* 创建一个映射内存,共享映射,包含两种数据,两个进程访问 ,上锁保护 */
 /* version 0.0.1 没有加进程锁 双进程++ */
-
+/* version 0.0.2 增加进程锁 */
 typedef struct{
 	int num;
 	pthread_mutex_t lock;
@@ -25,16 +25,24 @@ int main()
 	ftruncate(fd,sizeof(shared_t)); // 拓展文件大小至结构体大小 
 	ptr = mmap(NULL,sizeof(shared_t),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0); // 创建磁盘文件的硬盘映射
 	close(fd);
+	
+	/*初始化进程锁*/
+	pthread_mutexattr_t attr; // 定义锁属性
+	pthread_mutexattr_init(&attr); // 初始化锁属性
+	pthread_mutexattr_setpshared(&attr,PTHREAD_PROCESS_SHARED); // 设置锁属性为进程锁
+	pthread_mutex_init(&ptr->lock, &attr); // 使用锁属性初始化锁
 
 	ptr->num = 0;
 
 	pid_t pid;
 	pid = fork();
-	if(pid>0)
+	if(pid>0) 
 	{
 		for(int i=0;i<5000;i++)
 		{
+			pthread_mutex_lock(&ptr->lock); // 上锁
 			printf("parent process [%d] ++unm = %d\n",getpid(),++(ptr->num));
+			pthread_mutex_unlock(&ptr->lock); // 解锁
 		}
 		wait(NULL);
 	}
@@ -42,7 +50,9 @@ int main()
 	{
 		for(int i=0;i<5000;i++)
 		{
+			pthread_mutex_lock(&ptr->lock); // 上锁
 			printf("child process [%d] ++unm = %d\n",getpid(),++(ptr->num));
+			pthread_mutex_unlock(&ptr->lock); // 解锁
 		}
 		exit(0);
 
@@ -53,6 +63,7 @@ int main()
 		exit(0);
 	}
 	pthread_mutex_destroy(&ptr->lock);
+	pthread_mutexattr_destroy(&attr); // 销毁锁属性
 	munmap(ptr,sizeof(shared_t)); // 关闭磁盘映射
 	return 0;
 }
